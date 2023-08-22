c3f11cde03eadcc6ab5f73e83c0f8373a0d95d5b
shikong <919411476@qq.com>
2023-08-22 10:38:24 +0800

    测试

---
+++ api/src/main/java/cn/skcks/docking/gb28181/config/WebConfig.java
@@ -0,0 +1,10 @@
+package cn.skcks.docking.gb28181.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig {
+}
--- starter/src/test/java/cn/skcks/docking/gb28181/SipTest.java
+++ starter/src/test/java/cn/skcks/docking/gb28181/SipTest.java
@@ -3,12 +3,17 @@ package cn.skcks.docking.gb28181;
 import cn.skcks.docking.gb28181.core.sip.service.SipService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sip.SipProvider;
 
 @Slf4j
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 @SpringBootTest(classes = Gb28181DockingPlatformApplicationTest.class)
 public class SipTest {
     @Autowired
@@ -16,7 +21,7 @@ public class SipTest {
 
     @Test
     void context(){
-        SipProvider provider = service.getProvider("TCP", "10.10.10.20");
+        SipProvider provider = service.getProvider("TCP", "192.168.10.195");
         log.info("{}", provider);
     }
 }
