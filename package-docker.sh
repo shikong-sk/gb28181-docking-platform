4d459187f94c5d5ad097d3e9b84e076e86e01975
shikong <919411476@qq.com>
2023-08-27 21:29:05 +0800

    添加一键打包 docker 脚本 完善 README.md

--- package-docker.sh
+++ package-docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# 用户缓存打包过程下载的依赖
+mkdir repository
+curDir=`pwd`
 docker run --name maven --rm \
-	-v /mnt/sk-w/run/src/gb28181-docking-platform:/usr/src/mymaven \
-	-v /mnt/sk-w/run/docker/maven/repository:/root/.m2/repository \
-	-v /mnt/sk-w/run/docker/maven/settings.xml:/usr/share/maven/ref/settings.xml \
+	-v ${curDir}:/usr/src/mymaven \
+	-v ${curDir}/repository:/root/.m2/repository \
+	-v ${curDir}/settings.xml:/usr/share/maven/ref/settings.xml \
 	-v /etc/docker/daemon.json:/etc/docker/daemon.json -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker \
 	-w /usr/src/mymaven \
 	maven:3.9.3-eclipse-temurin-17 \
