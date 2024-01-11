648821525b222a46237c7ded5de2b4ac0f787f26
shikong <919411476@qq.com>
2024-01-11 09:11:53 +0800

    manscdp RecordInfo 请求 字段补充

--- gb28181-sip/src/main/java/cn/skcks/docking/gb28181/sip/manscdp/recordinfo/request/RecordInfoRequestDTO.java
+++ gb28181-sip/src/main/java/cn/skcks/docking/gb28181/sip/manscdp/recordinfo/request/RecordInfoRequestDTO.java
@@ -19,7 +19,7 @@ import java.util.Date;
 @Builder
 public class RecordInfoRequestDTO {
     /**
-     * 命令类型:设备信息查询(必选)
+     * 命令类型:设备录像查询(必选)
      */
     @Builder.Default
     private String cmdType = CmdType.RECORD_INFO;
@@ -42,9 +42,26 @@ public class RecordInfoRequestDTO {
     @JsonFormat(pattern = GB28181Constant.DATETIME_FORMAT, timezone = GB28181Constant.TIME_ZONE)
     private Date endTime;
 
+    private String filePath;
+
+    private String address;
+
     @Builder.Default
     private Integer Secrecy = 0;
 
     @Builder.Default
     private String type = "all";
+
+    @JacksonXmlProperty(localName = "RecorderID")
+    private String recorderId;
+
+    /**
+     * 录像模糊查询属性(可选) 缺省为 0; <p/>
+     * 0: 不进行模糊查询,此时根据SIP消息中To头域
+     * URI 中的ID值确定查询录像位置,若 ID 值为本域系统 ID 则进行中心历史记录检索
+     * 若为前端设备 ID 则进行前端设备历史记录检索; <p/>
+     *
+     * 1: 进行模糊查询,此时设备所在域应同时进行中心检索和前端检索并将结果统一返回
+     */
+    private Integer IndistinctQuery;
 }
